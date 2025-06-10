50ebf793eb4017e3548c07df9b3b68650f51577a
luanwillianzh <74968256+luanwillianzh@users.noreply.github.com>
2025-06-09 22:49:34 -0300

    Update index.py

--- src/index.py
+++ src/index.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import requests
@@ -11,21 +10,19 @@ app = FastAPI()
 def get_novel_info(novel):
     response = requests.get(f"https://centralnovel.com/series/{novel}/", verify=False)
     soup = BeautifulSoup(response.text, 'html.parser')
-    name = soup.select_one("h1[itemprop=name]").text.strip().replace("\n", " ")
+    name = soup.select_one("h1[itemprop=name]").text.strip()
     desc = soup.select_one(".entry-content p").text.strip()
     cover = soup.select_one("div.thumb img")["src"]
-    chapters = [a["href"].split("/")[-2] for a in soup.select("div.bixbox.bxcl.epcheck a")][::-1][:-2]
+    lista = soup.select("div.bixbox.bxcl.epcheck a")[::-1][:-2]
+    chapters = [ (" - ".join([ i.text for i in a.select("div")[:2] ]), a['href'].split('/')[-2]) for a in lista ]
     return {"nome": name, "desc": desc, "cover": cover, "chapters": chapters}
 
 @app.get("/chapter/{chapter}")
 def get_chapter(chapter):
     response = requests.get(f"https://centralnovel.com/{chapter}/", verify=False)
     soup = BeautifulSoup(response.text, 'html.parser')
-    title = soup.select_one("h1.entry-title").text.strip().replace("\n", " ")
-    try:
-      subtitle = soup.select_one("div.cat-series").text.strip().replace("\n", " ")
-    except:
-      subtitle = ""
+    title = soup.select_one("h1.entry-title").text.strip()
+    subtitle = soup.select_one("div.cat-series").text.strip()
     content = str(soup.select_one("div.epcontent.entry-content"))
     return {"title": title, "subtitle": subtitle, "content": content}
 
@@ -37,7 +34,4 @@ def search(text):
     data = "action=ts_ac_do_search&ts_ac_query={}".format(query)
     resp = requests.post(url, headers=headers, data=data, verify=False).json()
     lista = resp['series'][0]['all']
-    if lista == []:
-      return {"sucesso": False}
-    else:
-      return {"sucesso": True, "resultado": [ {"nome": a['post_title'], "url": a['post_link'].split("/")[-2], "cover": a['post_image']} for a in lista ]}
+    return {"sucesso": True, "resultado": [ {"nome": a['post_title'], "url": a['post_link'].split("/")[-2], "cover": a['post_image']} for a in lista ]}
